046d2ef4cd1fdadd74f3fb7c4582e8aa6551ef1c
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2025-01-15 23:28:29 +0100

    Got some success

--- src/SuperServices/ProjectService.cs
+++ src/SuperServices/ProjectService.cs
@@ -3,6 +3,7 @@ using System.Reactive.Linq;
 using Angor.Shared.Models;
 using Angor.Shared.Services;
 using DynamicData;
+using Nostr.Client.Messages.Metadata;
 
 namespace SuperServices;
 
@@ -19,19 +20,35 @@ public class ProjectService
 
     public IObservable<IChangeSet<ProjectData, string>> Connect()
     {
-        return Observable.FromAsync(() => _indexerService
+        var tuples = Observable.FromAsync(() => _indexerService
                 .GetProjectsAsync(null, 21))
-            .SelectMany(projectIndexerDatas => GetProjectMetadatas(projectIndexerDatas).ToList().Select(list => new { IndexerDatas = projectIndexerDatas, NostrDatas = list }))
-            .Select(arg => arg.IndexerDatas.Join(arg.NostrDatas, x => x.ProjectIdentifier, x => x.ProjectIdentifier, (x, y) => new ProjectData()
-            {
-                IndexerData = x,
-                ProjectInfo = y,
-                NostrMetadata = null,
-            }))
-            .ToObservableChangeSet(x => x.IndexerData.ProjectIdentifier);
+            .SelectMany(projectIndexerDatas => GetProjectInfo(projectIndexerDatas)
+                .ToList()
+                .SelectMany(projectInfos => GetProjectMetadatas(projectInfos).ToList().Select(metadatas => new
+                {
+                    metadatas, projectInfos, projectIndexerDatas
+                })));
+
+        var observable = tuples.Select(x =>
+        {
+            var infoAndMetadata = x.projectInfos.Join(x.metadatas, projectInfo => projectInfo.NostrPubKey, tuple => tuple.Item1, (info, tuple) => (info: info, Metadata: tuple.Item2));
+            return x.projectIndexerDatas
+                .Join(
+                    infoAndMetadata,
+                    projectIndexerData => projectIndexerData.ProjectIdentifier,
+                    tuple => tuple.info.ProjectIdentifier,
+                    (projectIndexerData, tuple) => new ProjectData
+                    {
+                        ProjectInfo = tuple.info,
+                        NostrMetadata = tuple.Metadata,
+                        IndexerData = projectIndexerData,
+                    });
+        });
+
+        return observable.ToObservableChangeSet(x => x.IndexerData.ProjectIdentifier);
     }
 
-    private IObservable<ProjectInfo> GetProjectMetadatas(List<ProjectIndexerData> list)
+    private IObservable<ProjectInfo> GetProjectInfo(IList<ProjectIndexerData> list)
     {
         return Observable.Create<ProjectInfo>(observer =>
         {
@@ -40,6 +57,16 @@ public class ProjectService
                 observer.OnCompleted,
                 list.Select(x => x.NostrEventId).ToArray()
             );
+
+            return Disposable.Empty;
+        });
+    }
+
+    private IObservable<(string, ProjectMetadata)> GetProjectMetadatas(IList<ProjectInfo> list)
+    {
+        return Observable.Create<(string, ProjectMetadata)>(observer =>
+        {
+            _relayService.LookupNostrProfileForNPub((npub, nostrMetadata) => observer.OnNext((npub, nostrMetadata)), () => observer.OnCompleted(), list.Select(x => x.NostrPubKey).ToArray());
 
             return Disposable.Empty;
         });
